18c04960c95d4c62777d179ccf9426fc1c71f0fb
semvs <semvs@ayvansaray.edu.tr>
2018-12-11 14:28:47 +0300

    filestream

---
+++ filestream/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace filestream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adres = @"C:\TestDizini\NewFile.txt";
+            FileStream fs = new FileStream(adres, FileMode.OpenOrCreate);
+            FileStream fs2 = new FileStream(adres, FileMode.Append,FileAccess.Write);
+            FileStream fs3 = new FileStream(adres, FileMode.Truncate, FileAccess.Write,FileShare.None);
+            //filestream akımının tamponunu temizler
+            //hafızadaki tampon bölgedeki bilgileri hemen diske yazar.
+            fs.Flush();
+            //Filestream tarafından kullanılan kaynakları boşaltır.
+            //tampon bölgeleri diske yazar serbesr bırakır.
+            //sistem kaynaklarını serbest bırakıyor.
+            //file close yapılmadan başkaları file i açıp işslem yapamaz.
+            fs.Close();
+
+        }
+    }
+}
